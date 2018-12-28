1d92bbdf06c2bbc75f86fc77c55ffc3b9df7d636
Kalab Oster <kalaboster@wordsum.com>
2018-12-27 17:05:45 -0800

    Write test for success call to build. Use junit for all tests.

--- src/test/java/com/scoring/datastore/service/StoreScoringServiceTests.java
+++ src/test/java/com/scoring/datastore/service/StoreScoringServiceTests.java
@@ -1,21 +1,27 @@
 package com.scoring.datastore.service;
 
+import com.scoring.datastore.model.ScoringModel;
 import com.scoring.datastore.utils.FileUtil;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.List;
 
+@RunWith(SpringRunner.class)
 public class StoreScoringServiceTests {
 
-    StoreScoringService storeScoringService;
+    static StoreScoringService storeScoringService;
 
-    @BeforeTest
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         storeScoringService = new StoreScoringService();
     }
 
@@ -31,9 +37,21 @@ public class StoreScoringServiceTests {
         Assert.assertEquals(234, file.length());
     }
 
-    @Test(expectedExceptions = ScoringServiceException.class)
+    @Test(expected = ScoringServiceException.class)
     public void testTransformNullFile() {
         storeScoringService.transform(null);
+    }
+
+    @Test
+    public void testBuildSuccess() throws Exception {
+        File testFile = FileUtil.readResourceFile("/datastoreInput.txt");
+        byte[] fileBytes = Files.readAllBytes(testFile.toPath());
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("datastoreInput", "out/test/datastoreInput.txt", MediaType.TEXT_PLAIN_VALUE, fileBytes);
+
+        File file = storeScoringService.transform(mockMultipartFile);
+        List<ScoringModel> scoringModels = storeScoringService.build(file);
+
+        Assert.assertEquals("2014-04-01", scoringModels.get(0).getDate());
     }
 
 }
