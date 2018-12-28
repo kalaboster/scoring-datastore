97043039a96d58657f2e2ade0c1b61694c7eebef
Kalab Oster <kalaboster@wordsum.com>
2018-12-27 21:13:10 -0800

    Implement a validator for the fields.

---
+++ src/main/java/com/scoring/datastore/model/ScoringValidatorBuilder.java
@@ -0,0 +1,74 @@
+package com.scoring.datastore.model;
+
+public class ScoringValidatorBuilder implements ScoringBuilder {
+
+    private ScoringModel scoringModel;
+
+    ScoringValidatorBuilder(ScoringModel scoringModel) {
+        this.scoringModel = scoringModel;
+    }
+
+    @Override
+    public void validateStb() {
+        if (this.scoringModel.getStb() == null) {
+            throw new ScoreValidatorException("STB is null.");
+        }
+        if (this.scoringModel.getStb().length() > 63) {
+            throw new ScoreValidatorException("STB length needs to be 64 characgters not: " + this.scoringModel.getStb().length() + 1);
+        }
+    }
+
+    @Override
+    public void validateTitle() {
+        if (this.scoringModel.getTitle() == null) {
+            throw new ScoreValidatorException("Title is null.");
+        }
+        if (this.scoringModel.getTitle().length() > 63) {
+            throw new ScoreValidatorException("Title length needs to be 64 characgters not: " + this.scoringModel.getTitle().length() + 1);
+        }
+    }
+
+    @Override
+    public void validateProvidor() {
+        if (this.scoringModel.getProvidor() == null) {
+            throw new ScoreValidatorException("Provider is null.");
+        }
+        if (this.scoringModel.getProvidor().length() > 63) {
+            throw new ScoreValidatorException("Providor length needs to be 64 characgters not: " + this.scoringModel.getProvidor().length() + 1);
+        }
+    }
+
+
+    @Override
+    public void validateDate() {
+        if (this.scoringModel.getDate() == null) {
+            throw new ScoreValidatorException("Date is null.");
+        }
+        if (!this.scoringModel.getDate().matches("(\\d\\d\\d\\d-\\d\\d-\\d\\d)")) {
+            throw new ScoreValidatorException("Date doesn't match YYYY-MM-DD");
+        }
+    }
+
+
+    @Override
+    public void validateRev() {
+        if (this.scoringModel.getRev() == null) {
+            throw new ScoreValidatorException("Rev is null.");
+        }
+        if (!this.scoringModel.getRev().matches("(\\d)*\\.\\d\\d")) {
+            throw new ScoreValidatorException("Date doesn't match USD money pattern");
+        }
+    }
+
+
+    @Override
+    public void validateViewTime() {
+        if (this.scoringModel.getViewTime() == null) {
+            throw new ScoreValidatorException("Rev is null.");
+        }
+        if (!this.scoringModel.getViewTime().matches("(\\d)*:\\d\\d")) {
+            throw new ScoreValidatorException("Date doesn't match time pattern hours:minutes");
+        }
+    }
+
+}
