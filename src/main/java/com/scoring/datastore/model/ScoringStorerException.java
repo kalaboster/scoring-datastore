afe629a795f529016fb496b32c5e59d6de10b797
Kalab Oster <kalaboster@wordsum.com>
2018-12-28 13:46:35 -0800

    Write exception for the ScoringStorer to identify.

---
+++ src/main/java/com/scoring/datastore/model/ScoringStorerException.java
@@ -0,0 +1,8 @@
+package com.scoring.datastore.model;
+
+public class ScoringStorerException extends RuntimeException {
+
+    ScoringStorerException(String message) {
+        super(message);
+    }
+}
