a5ed736f3d040d9037ff054a6b4073b7f1f6510c
Kalab Oster <kalaboster@wordsum.com>
2018-12-28 13:49:37 -0800

    Write tests for Scoring Model Storer.

---
+++ src/test/java/com/scoring/datastore/model/ScoringModelStorerTests.java
@@ -0,0 +1,43 @@
+package com.scoring.datastore.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScoringModelStorerTests {
+
+    final String testDir = "./out/test-datastore";
+
+    @Test
+    public void testInitSuccess() throws Exception {
+
+        ScoringModel scoringModel = new ScoringModel();
+        scoringModel.setTitle("Title");
+        scoringModel.setStb("ShortSTB");
+        scoringModel.setProvidor("Provider");
+        scoringModel.setDate("2014-04-03");
+        scoringModel.setViewTime("2:40");
+        scoringModel.setRev("12.00");
+
+        ScoringStorer scoringStorer = new ScoringModelStorer();
+        Assert.assertTrue(scoringStorer.init(testDir));
+    }
+
+    @Test
+    public void testMakeRecordSuccess() throws Exception {
+
+        ScoringModel scoringModel = new ScoringModel();
+        scoringModel.setTitle("Title");
+        scoringModel.setStb("ShortSTB");
+        scoringModel.setProvidor("Provider");
+        scoringModel.setDate("2014-04-03");
+        scoringModel.setViewTime("2:40");
+        scoringModel.setRev("12.00");
+
+        ScoringStorer scoringStorer = new ScoringModelStorer();
+        Assert.assertTrue(scoringStorer.init(testDir));
+        scoringStorer.makeRecord(scoringModel, testDir);
+
+    }
+
+
+}
