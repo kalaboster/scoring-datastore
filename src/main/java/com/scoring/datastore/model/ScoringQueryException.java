3839cf9d93c220d5127a06a96d9efc09ccad6064
Kalab Oster <kalaboster@wordsum.com>
2018-12-30 10:42:30 -0800

    Add the exception.

---
+++ src/main/java/com/scoring/datastore/model/ScoringQueryException.java
@@ -0,0 +1,8 @@
+package com.scoring.datastore.model;
+
+public class ScoringQueryException extends RuntimeException {
+
+    ScoringQueryException(String message) {
+        super(message);
+    }
+}
