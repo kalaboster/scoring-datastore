ac1577ea9ebcbbf664f52f69dcd084e5334ba453
Kalab Oster <kalaboster@wordsum.com>
2018-12-31 10:27:45 -0800

    Write load path and file into the ScoringModel.

--- src/main/java/com/scoring/datastore/model/ScoringQuery.java
+++ src/main/java/com/scoring/datastore/model/ScoringQuery.java
@@ -1,5 +1,10 @@
 package com.scoring.datastore.model;
 
+import com.google.gson.Gson;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,15 +41,91 @@ public class ScoringQuery implements ScoringQueryBuilder {
     @Override
     public List<ScoringModel> loadStore(String pathStore) {
 
+        List<ScoringModel> scoringModels = new LinkedList<>();
 
-        return new LinkedList<>();
+        try {
+            for (Path path : Files.newDirectoryStream(Paths.get(pathStore),
+                    path -> path.toString().endsWith(".json")).iterator().next()) {
+                ScoringModel scoringModel = new ScoringModel();
+
+                //loadFilePath(scoringModel, "string", pathStore);
+                //loodFile(scoringModel, "string");
+
+                scoringModels.add(scoringModel);
+            }
+
+        } catch (Exception e) {
+            throw new ScoringQueryException("error");
+        }
+
+        return scoringModels;
     }
 
     @Override
-    public List<ScoringQueryModel> query(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
+    public List<ScoringQueryModel> select(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
 
 
         return new LinkedList<ScoringQueryModel>();
+    }
+
+    @Override
+    public List<ScoringQueryModel> order(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
+
+
+        return new LinkedList<ScoringQueryModel>();
+    }
+
+    @Override
+    public List<ScoringQueryModel> filter(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
+
+
+        return new LinkedList<ScoringQueryModel>();
+    }
+
+
+    protected void loadFilePath(ScoringModel scoringModel, String file, String dbName) {
+
+        String[] parts = file.split(dbName);
+        String[] strings = parts[1].split("(/|_)");
+        scoringModel.setProvider(strings[1]);
+        scoringModel.setStb(strings[2]);
+        scoringModel.setTitle(strings[3]);
+        scoringModel.setDate(strings[4].replace(".json", ""));
+
+    }
+
+    protected void loadFile(ScoringModel scoringModel, String file) {
+
+        try {
+            String carInfoJson = new String(Files.readAllBytes(Paths.get(file.replace("%20", " ")))).replace("%20", " ");
+            Gson gson = new Gson();
+            ScoringFile scoringFile = gson.fromJson(carInfoJson, ScoringQuery.ScoringFile.class);
+            scoringModel.setViewTime(scoringFile.getViewTime());
+            scoringModel.setRev(scoringFile.getRev());
+        } catch (Exception e) {
+            throw new ScoringQueryException("loodFile error.");
+        }
+    }
+
+    class ScoringFile {
+        String rev;
+        String viewTime;
+
+        public String getRev() {
+            return rev;
+        }
+
+        public void setRev(String rev) {
+            this.rev = rev;
+        }
+
+        public String getViewTime() {
+            return viewTime;
+        }
+
+        public void setViewTime(String viewTime) {
+            this.viewTime = viewTime;
+        }
     }
 
 }
