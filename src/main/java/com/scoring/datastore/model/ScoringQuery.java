fd20b32a93dfaa2431f5716eef59c4ef99c947a9
Kalab Oster <kalaboster@wordsum.com>
2019-01-01 09:33:34 -0800

    Write filter.

--- src/main/java/com/scoring/datastore/model/ScoringQuery.java
+++ src/main/java/com/scoring/datastore/model/ScoringQuery.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ScoringQuery implements ScoringQueryBuilder {
 
@@ -67,22 +68,22 @@ public class ScoringQuery implements ScoringQueryBuilder {
     @Override
     public List<ScoringModel> select(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
 
-        if(!scoringQueryModel.getSelect().isDate()){
+        if (!scoringQueryModel.getSelect().isDate()) {
             scoringModels.forEach(s -> s.setDate(null));
         }
-        if(!scoringQueryModel.getSelect().isProvider()){
+        if (!scoringQueryModel.getSelect().isProvider()) {
             scoringModels.forEach(s -> s.setProvider(null));
         }
-        if(!scoringQueryModel.getSelect().isRev()){
+        if (!scoringQueryModel.getSelect().isRev()) {
             scoringModels.forEach(s -> s.setRev(null));
         }
-        if(!scoringQueryModel.getSelect().isStb()){
+        if (!scoringQueryModel.getSelect().isStb()) {
             scoringModels.forEach(s -> s.setStb(null));
         }
-        if(!scoringQueryModel.getSelect().isTitle()){
+        if (!scoringQueryModel.getSelect().isTitle()) {
             scoringModels.forEach(s -> s.setTitle(null));
         }
-        if(!scoringQueryModel.getSelect().isViewTime()){
+        if (!scoringQueryModel.getSelect().isViewTime()) {
             scoringModels.forEach(s -> s.setViewTime(null));
         }
 
@@ -96,10 +97,28 @@ public class ScoringQuery implements ScoringQueryBuilder {
     }
 
     @Override
-    public List<ScoringModel> filter(ScoringQueryModel scoringQueryModel, List<ScoringModel> scoringModels) {
+    public List<ScoringModel> filter(ScoringQueryModel scoringQueryModel, List<ScoringModel> filteredScoringModels) {
 
+        if (scoringQueryModel.getSelect().isDate() && scoringQueryModel.getFilter().getMap().keySet().contains("DATE")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getDate().equals(scoringQueryModel.getFilter().getMap().get("DATE")))).collect(Collectors.toList()));
+        }
+        if (scoringQueryModel.getSelect().isProvider() && scoringQueryModel.getFilter().getMap().keySet().contains("PROVIDER")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getProvider().equals(scoringQueryModel.getFilter().getMap().get("PROVIDER")))).collect(Collectors.toList()));
+        }
+        if (scoringQueryModel.getSelect().isRev() && scoringQueryModel.getFilter().getMap().keySet().contains("REV")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getRev().equals(scoringQueryModel.getFilter().getMap().get("REV")))).collect(Collectors.toList()));
+        }
+        if (scoringQueryModel.getSelect().isStb() && scoringQueryModel.getFilter().getMap().keySet().contains("STB")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getStb().equals(scoringQueryModel.getFilter().getMap().get("STB")))).collect(Collectors.toList()));
+        }
+        if (scoringQueryModel.getSelect().isTitle() && scoringQueryModel.getFilter().getMap().keySet().contains("TITLE")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getTitle().equals(scoringQueryModel.getFilter().getMap().get("TITLE")))).collect(Collectors.toList()));
+        }
+        if (scoringQueryModel.getSelect().isViewTime() && scoringQueryModel.getFilter().getMap().keySet().contains("VIEW_TIME")) {
+            filteredScoringModels.removeAll(filteredScoringModels.stream().filter((f -> !f.getViewTime().equals(scoringQueryModel.getFilter().getMap().get("VIEW_TIME")))).collect(Collectors.toList()));
+        }
 
-        return new LinkedList<ScoringModel>();
+        return filteredScoringModels;
     }
 
 
