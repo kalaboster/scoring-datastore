9125a1c1db488afea4d8f9a253884ba8bbb32d36
Kalab Oster <kalaboster@wordsum.com>
2018-12-31 21:43:27 -0800

    Write tests for the complete load of scoring models.

--- src/test/java/com/scoring/datastore/model/ScoringQueryTests.java
+++ src/test/java/com/scoring/datastore/model/ScoringQueryTests.java
@@ -1,11 +1,12 @@
 package com.scoring.datastore.model;
 
-import com.scoring.datastore.utils.FileUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
+
+import static com.scoring.datastore.utils.FileUtil.readResourceFile;
 
 public class ScoringQueryTests {
 
@@ -23,11 +24,11 @@ public class ScoringQueryTests {
     @Test
     public void testLoadPath() throws Exception {
 
-        File file = FileUtil.readResourceFile("/scoring-datastore-test/buena vista/stb1_unbreakable_2014-04-03.json");
+        File file = readResourceFile("/rootdir/scoring-datastore-test/buena vista/stb1_unbreakable_2014-04-03.json");
         ScoringQuery scoringQuery = new ScoringQuery();
         ScoringModel scoringModel = new ScoringModel();
 
-        scoringQuery.loadFilePath(scoringModel, file.getAbsolutePath(), "scoring-datastore-test");
+        scoringQuery.loadFilePath(scoringModel, file.getAbsolutePath(), "rootdir/scoring-datastore-test");
 
         Assert.assertEquals("2014-04-03", scoringModel.getDate());
     }
@@ -35,7 +36,7 @@ public class ScoringQueryTests {
     @Test
     public void testLoadFile() throws Exception {
 
-        File file = FileUtil.readResourceFile("/scoring-datastore-test/buena vista/stb1_unbreakable_2014-04-03.json");
+        File file = readResourceFile("/rootdir/scoring-datastore-test/buena vista/stb1_unbreakable_2014-04-03.json");
         ScoringQuery scoringQuery = new ScoringQuery();
         ScoringModel scoringModel = new ScoringModel();
 
@@ -44,12 +45,14 @@ public class ScoringQueryTests {
         Assert.assertEquals("2:05", scoringModel.getViewTime());
     }
 
-    //@Test
-    public void testLoadStoreSuccess() {
+    @Test
+    public void testLoadStoreSuccess() throws Exception {
 
+        File file = readResourceFile("/rootdir");
         ScoringQuery scoringQuery = new ScoringQuery();
-        List<ScoringModel> scoringModels = scoringQuery.loadStore("/home/kalab/github/scoring-datastore-test/src/test/resources/scoring-datastore-test");
+        List<ScoringModel> scoringModels = scoringQuery.loadStore(file.getAbsolutePath(), "/scoring-datastore-test");
 
+        Assert.assertEquals("1:05", scoringModels.get(0).getViewTime());
     }
 
 }
