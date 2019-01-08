bdbf28044e4b8980998be850b20736b564dbec2f
Kalab Oster <kalaboster@wordsum.com>
2019-01-07 22:25:29 -0800

    Add some logging.

--- src/main/java/com/scoring/datastore/controller/ScoringController.java
+++ src/main/java/com/scoring/datastore/controller/ScoringController.java
@@ -4,6 +4,8 @@ import com.scoring.datastore.model.ScoringModel;
 import com.scoring.datastore.model.ScoringQuery;
 import com.scoring.datastore.model.ScoringQueryModel;
 import com.scoring.datastore.service.ScoringService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,8 @@ import java.util.List;
 @RestController
 public class ScoringController {
 
+    private static final Logger log = LoggerFactory.getLogger(ScoringController.class);
+
     private transient ScoringService scoringService;
 
     @Autowired
@@ -23,6 +27,8 @@ public class ScoringController {
 
     @RequestMapping(value = "/scoring/query", method = RequestMethod.POST)
     public List<ScoringModel> query(@RequestBody ScoringQueryModel scoringQueryModel) {
+
+        log.info("Scoring Query: " + scoringQueryModel.getSelect());
 
         ScoringQuery scoringQuery = new ScoringQuery();
         if (scoringQueryModel == null) {
